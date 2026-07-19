d5b938de136ae3c9be5b6f0f2f5cfb600689e66e
Christopher De Leon <chrisdeleon.dev@gmail.com>
2026-07-18 20:20:23 -0400

    add initial Tamagui configuration

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text } from "react-native";
+import { TamaguiProvider, View, createTamagui } from "@tamagui/core";
+import { defaultConfig } from "@tamagui/config/v5";
+
+const config = createTamagui(defaultConfig);
+
+type Conf = typeof config;
+
+// make imports typed
+declare module "@tamagui/core" {
+  interface TamaguiCustomConfig extends Conf {}
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TamaguiProvider config={config} defaultTheme="light">
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </TamaguiProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
